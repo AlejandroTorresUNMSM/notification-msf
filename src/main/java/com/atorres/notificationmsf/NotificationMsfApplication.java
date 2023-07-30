7eba87e99cb09c4d5998c02a64d4d00b2c314afd
AlejandroTorresUNSMS <alephdmc2575@gmail.com>
2023-07-30 16:16:14 -0500

    dev: commit init

---
+++ src/main/java/com/atorres/notificationmsf/NotificationMsfApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.notificationmsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationMsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationMsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atorres/notificationmsf/NotificationMsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atorres.notificationmsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationMsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
